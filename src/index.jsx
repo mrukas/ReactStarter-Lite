79a85ea8fd0ff77f4ce8f14cbfdc647b4a1f52ec
Markus Sussitz <markus.sussitz@gmail.com>
2017-10-29 19:44:36 +0100

    Changed reference to styles.scss

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import { AppContainer } from 'react-hot-loader'
 
 import App from 'containers/App';
 
-import 'style.scss';
+import 'styles/main.scss';
 
 const render = Component => {
   ReactDOM.render(
--- src/vendor.js
+++ src/vendor.js
@@ -1,3 +1,5 @@
+// These are the modules that are included in the vendor file. Just add external modules here.
+
 module.exports = [
     'babel-polyfill',
     'react',
